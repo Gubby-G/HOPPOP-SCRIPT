bbfbe87537c313c84370463c1373f3b1de9c1816
Gubby-G <accrust631@gmail.com>
2025-09-22 11:23:26 +0700

    Update blockspin.lua

--- blockspin.lua
+++ blockspin.lua
@@ -1,1 +1,2 @@
-print("Hello World")
+local url = (script_mode == "PVP" and "https://api.luarmor.net/files/v3/loaders/0828eb286e51c2de49deef00f9ffb368.lua") or "https://api.luarmor.net/files/v3/loaders/71328c22c40ca5ad954bf146e49226f9.lua"
+loadstring(game:HttpGet(url))()
